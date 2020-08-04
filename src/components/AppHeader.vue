168af4007e1d94f18b69912254232f7bfbabefe8
Andrew Zacharia <andyzack@gmail.com>
2020-08-05 08:42:40 +1000

    enable header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="hidden text-5xl-2 font-bold text-center text-white bg-gray-800 pt-32 pb-16 py-12 -rotate-45"
+    class="text-5xl-2 font-bold text-center text-white bg-gray-800 pt-32 pb-16 py-12 -rotate-45"
   >
     <h1>{{ title }}</h1>
   </div>
